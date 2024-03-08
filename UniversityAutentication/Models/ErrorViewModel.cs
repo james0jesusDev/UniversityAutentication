375d358320ebe08b9419ba27df8600f13f53cc63
james0jesusDev <james0jesus@gmail.com>
2024-03-08 16:23:30 +0100

    Agregar archivos de proyecto.

---
+++ UniversityAutentication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniversityAutentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
